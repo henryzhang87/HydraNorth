f5361d72ef7c200b3cecc04adbb8fc12830b26f6
root <root@worthwhile-server.local>
2014-10-09 11:45:23 -0600

    initial commit
    created using 'rails generate worthwhile:install'

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'd42f3897352730764a58f8a17a6136482d7a2b122cf3bbf52ab183ea4761abe1e29c9928cda6eb7140ed1e8a9e260c9a8077d7b94998aba13ebee9933eca47b1'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Worthwhile::Application.config.secret_key_base = 'ecd15eafd71aff4acaa989032c0a7ed1b76c8a7f80f1cb52419cfec5237818d68c9aa9c9a52be5457ad56f6d14055942dce0ed8d764ce590d286a1fcaa965812'
