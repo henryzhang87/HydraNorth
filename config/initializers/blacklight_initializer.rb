c0a299dee958cd14e6ca96b2446a587fc8d9b61d
Weiwei Shi <weiwei.shi@ualberta.ca>
2014-11-26 16:06:52 -0700

    sufia4.2, fedora 4 and active fedora 9.0.0. beta2

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'd4fdce690fecb8121a39da7ea77d0370c1d38140138b23e031ab888bb197fe707a7f78949de01b13f738f696f3c186d5215e80ff9cd0b5b691e56e052feb415b'
+
