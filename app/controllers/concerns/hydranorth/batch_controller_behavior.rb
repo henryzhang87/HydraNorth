cc4cfcedf9caf6b7a15cb0b6e07715fa75e58143
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-02-13 11:54:20 -0700

    Update batch_controller_behavior.rb
    to allow proper "Save" function to the form.

--- app/controllers/concerns/hydranorth/batch_controller_behavior.rb
+++ app/controllers/concerns/hydranorth/batch_controller_behavior.rb
@@ -3,6 +3,21 @@ module Hydranorth
     extend ActiveSupport::Concern
     include Sufia::BatchControllerBehavior
     
+    def update
+      authenticate_user!
+      @batch = Batch.find_or_create(params[:id])
+      @batch.status = ["processing"]
+      @batch.save
+      file_attributes = Hydranorth::Forms::BatchEditForm.model_attributes(params[:generic_file])
+      Sufia.queue.push(BatchUpdateJob.new(current_user.user_key, params[:id], params[:title], file_attributes, params[:visibility]))
+      flash[:notice] = 'Your files are being processed by ' + t('sufia.product_name') + ' in the background. The metadata and access controls you specified are being applied. Files will be marked <span class="label label-danger" title="Private">Private</span> until this process is complete (shouldn\'t take too long, hang in there!). You may need to refresh your dashboard to see these updates.'
+      if uploading_on_behalf_of? @batch
+        redirect_to sufia.dashboard_shares_path
+      else
+        redirect_to sufia.dashboard_files_path
+      end
+    end
+    
     protected
     
     def edit_form
