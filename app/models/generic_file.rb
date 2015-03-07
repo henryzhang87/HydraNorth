c187dd8113c7b9ef66a7bbff98d3076524a710ce
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-03-07 08:49:24 -0700

    added authority control to field spatial

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -1,7 +1,3 @@
 class GenericFile < ActiveFedora::Base
   include Hydranorth::GenericFile
-   property :date_created, predicate: ::RDF::DC.created, multiple: false do |index|
-          index.as :stored_searchable
-        end
-  
 end
